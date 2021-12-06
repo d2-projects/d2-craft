52727f24646650100db3970467ce6d205febd179
CNine <cnine229@gmail.com>
2021-12-06 23:59:04 +0800

    chore: typed recursive generic type case

--- packages/typed/src/lib/typed.ts
+++ packages/typed/src/lib/typed.ts
@@ -1,3 +1,36 @@
 export function typed(): string {
   return 'typed';
 }
+
+interface NodeMetaBase<Name extends string, Config, _NodeMeta> {
+  component: Name,
+  config?: Config
+  children?: _NodeMeta[]
+}
+
+export interface RootMeta<_NodeMeta> {
+  version: string;
+  children: _NodeMeta[]
+}
+
+type NodeMeta
+  = NodeMetaBase<'A', { a: string }, NodeMeta>
+  | NodeMetaBase<'B', { b: string }, NodeMeta>
+  | NodeMetaBase<'C', { c: string }, NodeMeta>
+
+export const testTree: RootMeta<NodeMeta> = {
+  version: '1',
+  children: [{
+    component: 'A',
+    config: {
+      a: 'asd'
+    },
+    children: [
+      {component: 'B', config: {b: 'b'}, children: [
+        {component: 'C', config: {
+          c: 'c'
+        }}
+      ]}
+    ]
+  }]
+}
