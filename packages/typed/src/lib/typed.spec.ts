844b785a91b6f3f844a07b99ff551e760707efae
CNine <cnine229@gmail.com>
2021-12-07 09:19:03 +0800

    chore: move typed test

--- packages/typed/src/lib/typed.spec.ts
+++ packages/typed/src/lib/typed.spec.ts
@@ -1,7 +1,27 @@
-import { typed } from './typed';
+import { NodeMeta, NodeMetaBase, RootMeta } from './typed';
 
 describe('typed', () => {
   it('should work', () => {
-    expect(typed()).toEqual('typed');
+    type ExItem<N, C> = NodeMetaBase<N, C, NodeMeta<ExNodeMeta>>
+    type ExNodeMeta<Ex = null> = NodeMeta< Ex
+      | ExItem<'A', { a: string }>
+      | ExItem<'B', { b: string }>
+    >
+
+    const testTree: RootMeta<ExNodeMeta> = {
+      children: [{
+        component: 'A',
+        config: { a: '' },
+        children: [{
+          component: 'B',
+          config: { b: '' },
+          children: [{
+            component: 'A'
+          }]
+        }]
+      }]
+    }
+
+    expect(testTree).toBeTruthy();
   });
 });
--- packages/typed/src/lib/typed.ts
+++ packages/typed/src/lib/typed.ts
@@ -1,54 +1,34 @@
-export function typed(): string {
-  return 'typed';
-}
-
-interface NodeMetaBase<Name extends string, Config, _NodeMeta> {
+export interface NodeMetaBase<Name, Config, _NodeMeta> {
   component: Name,
   config?: Config
   children?: _NodeMeta[]
 }
 
-export interface RootMeta<_NodeMeta> {
-  version: string;
-  children: _NodeMeta[]
-}
+export type NodeMeta<Ex = null> = Ex
 
-type NodeMeta<Ex = null>
-  = NodeMetaBase<'A', { a: string }, NodeMeta<Ex>>
-  | NodeMetaBase<'B', { b: string }, NodeMeta<Ex>>
-  | NodeMetaBase<'C', { c: string }, NodeMeta<Ex>>
-  | Ex
-
-// extends
-type ExNodeMeta<Ex = null> = NodeMeta<
-    NodeMetaBase<'E', { e: string }, NodeMeta<ExNodeMeta>>
-  | NodeMetaBase<'F', { f: string }, NodeMeta<ExNodeMeta>>
-  | Ex
->
-
-// extends align
-type ExxNodeMeta = ExNodeMeta<
-    NodeMetaBase<'X', { x: string }, NodeMeta<ExxNodeMeta>>
-  | NodeMetaBase<'Y', { y: string }, NodeMeta<ExxNodeMeta>>
->
-
-export const testTree: RootMeta<ExxNodeMeta> = {
-  version: '1',
-  children: [{
-    component: 'A',
-    config: { a: 'asd' },
-    children: [
-      {component: 'B', config: {b: 'b'}, children: [
-        {component: 'C', config: { c: 'c' }},
-        {component: 'E', children: [
-          { component: 'B' },
-          { component: 'A', children: [{ component: 'E' }]}
-        ]}
-      ]}
-    ]
-  }, {
-    component: 'X',
-    config: { x: '' },
-    children: [{ component: 'A' }]
-  }]
+/**
+ * @example
+ *
+ *   type ExItem<N, C> = NodeMetaBase<N, C, NodeMeta<ExNodeMeta>>
+ *   type ExNodeMeta<Ex = null> = NodeMeta< Ex
+ *     | ExItem<'A', { a: string }>
+ *     | ExItem<'B', { b: string }>
+ *   >
+ *
+ *   const testTree: RootMeta<ExNodeMeta> = {
+ *     children: [{
+ *       component: 'A',
+ *       config: { a: '' },
+ *       children: [{
+ *         component: 'B',
+ *         config: { b: '' },
+ *         children: [{
+ *           component: 'A'
+ *         }]
+ *       }]
+ *     }]
+ *   }
+ */
+export interface RootMeta<ExtendableNodeMeta> {
+  children: ExtendableNodeMeta[]
 }
