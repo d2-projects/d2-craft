ffeda0289547f93d064c22373852774dc13657d5
CNine <cnine229@gmail.com>
2021-12-25 23:28:27 +0800

    chore: generate react app `example-react-antd-craft`

---
+++ packages/example-react-antd-craft/src/app/app.tsx
@@ -0,0 +1,41 @@
+import styles from './app.module.scss';
+
+import { Route, Link } from 'react-router-dom';
+
+export function App() {
+  return (
+    <div className={styles.app}>
+      <div role="navigation">
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/page-2">Page 2</Link>
+          </li>
+        </ul>
+      </div>
+      <Route
+        path="/"
+        exact
+        render={() => (
+          <div>
+            This is the generated root route.{' '}
+            <Link to="/page-2">Click here for page 2.</Link>
+          </div>
+        )}
+      />
+      <Route
+        path="/page-2"
+        exact
+        render={() => (
+          <div>
+            <Link to="/">Click here to go back to root page.</Link>
+          </div>
+        )}
+      />
+    </div>
+  );
+}
+
+export default App;
